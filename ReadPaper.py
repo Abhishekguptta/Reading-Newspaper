3fe8c9828ff6f7f84d7b08ba523716bff7554187
ABHISHEK GUPTA <87572737+Abhishekguptta@users.noreply.github.com>
2021-07-20 02:49:22 +0530

    Update ReadPaper.py

--- ReadPaper.py
+++ ReadPaper.py
@@ -2,8 +2,24 @@
 
 def speak(str):
     from win32com.client import Dispatch
-    speak =  Dispatch("SAPI.SpVoice")
-    speak.speak(str)
+    speak=Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+import requests
+import json
+url = ("https://newsapi.org/v2/everything?q=tesla&from=2021-06-19&sortBy=publishedAt&apiKey=b2943e6f5a3c4d4ead762bc6dd3c9ab9")
+resp = requests.get(url)
+txt = resp.text
+jos = json.loads(txt)
+
+try:
+    speak("The request was accepted ")
+    speak("Top news of the day")
+    speak(jos)
+except Exception as e:
+    speak("Sorry! the request was not accepted")
     
-if __name__ == '__main__':
-    speak("You are the number one AB.")    
+
+
+
+ 
