704c1dd6de258753ff010e1d786fd49017d23082
ABHISHEK GUPTA <87572737+Abhishekguptta@users.noreply.github.com>
2021-07-20 00:01:31 +0530

    Add files via upload

---
+++ ReadPaper.py
@@ -0,0 +1,9 @@
+#Reading Newspaper just something feels AI
+
+def speak(str):
+    from win32com.client import Dispatch
+    speak =  Dispatch("SAPI.SpVoice")
+    speak.speak(str)
+    
+if __name__ == '__main__':
+    speak("You are the number one.")    
